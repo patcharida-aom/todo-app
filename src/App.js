9f3be2e5f57169db0cd64b785ea2cbea10ab33f5
patcharida-aom <patcharida.aom@gmail.com>
2019-03-27 06:46:24 +0700

    Change BG + Input Head-Text, Text Field

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import TextField from '@material-ui/core/TextField';
+import classNames from 'classnames';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <h1>Todo-App</h1>
         </header>
+        <body className="App-body">
+          <TextField className="input-list"
+            label="What needs to be done?"
+            variant="outlined"
+          />
+        </body>
       </div>
     );
   }
