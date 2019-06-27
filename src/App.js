63ed1e0d84c54915d51a2b90728629df67a51f55
patcharida-aom <patcharida.aom@gmail.com>
2019-06-27 13:12:54 +0700

    update code

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import React, { Component } from 'react';
 import './App.css';
 import TextField from '@material-ui/core/TextField';
-import classNames from 'classnames';
+import Button from '@material-ui/core/Button';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { item: [], text: ''}
+  }
+  handleChange = (event) => {
+    this.setState({text: event.target.value})
+  }
+  onSubmit = (event) =>{
+    event.preventDefault()
+    this.setState({item: [this.state.item, this.state.text],
+    text: ''})
+  }
+  
   render() {
     return (
       <div className="App">
@@ -11,10 +24,23 @@ class App extends Component {
           <h1>Todo-App</h1>
         </header>
         <body className="App-body">
-          <TextField className="input-list"
-            label="What needs to be done?"
-            variant="outlined"
-          />
+          
+          <form onSubmit={this.onSubmit}>
+            <TextField className="input-list"
+              onChange={this.handleChange}
+              label="What needs to be done?"
+              variant="outlined"
+            />
+            <p/>
+            <button variant="extendedFab"
+            size="large"
+            onClick = {this.onSubmit}
+            >
+              Enter
+            </button>
+          </form>
+          <p>{this.state.item}</p>
+          
         </body>
       </div>
     );
